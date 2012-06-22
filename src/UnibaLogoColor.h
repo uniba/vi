379cbc28f4b6883f1569bb5ea483f881ed776def
moxus <moxuse@gmail.com>
2012-06-22 12:49:00 +0900

    data（外部リソース..）を追加

---
+++ src/UnibaLogoColor.h
@@ -0,0 +1,14 @@
+//
+//  UnibaLogoColor.h
+//  UnibaMotionLogo
+//
+//  Created by mori koichiro on 12/06/22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef UnibaMotionLogo_UnibaLogoColor_h
+#define UnibaMotionLogo_UnibaLogoColor_h
+
+
+
+#endif
